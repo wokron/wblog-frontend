0ea84ab565890a1fa17981e072f65221ae1cfeb3
wokron <stringcatwok@gmail.com>
2023-10-16 21:52:19 +0800

    feat: show tag in article

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,18 +1,24 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { ArticleContent } from "../components/Article";
 import { CommentForm, PageComment } from "../components/Comment";
 import { useEffect, useState } from "react";
 import client from "../utils/client";
+import Tag from "../components/Tag";
 
 
 function Article() {
+    const navigate = useNavigate();
     const { id } = useParams();
     const [totalPages, setTotalPages] = useState(1);
     const [article, setArticle] = useState({
         id: 0,
         title: "Lorem ipsum dolor sit amet",
+        tags: [{
+            id: 0,
+            name: "ipsum dolor",
+        }],
         category: {
-            id: 1,
+            id: 0,
             name: "consectetur",
         },
         create_time: "2023.12.31",
@@ -61,12 +67,25 @@ function Article() {
         setSearchParams(searchParams);
     }
 
+    function handleTagClick(id: number) {
+        navigate(`/search/?tag_ids=${id}`);
+    }
+
     return (
         <section className="section pt-5">
             <div className="columns is-centered">
                 <div className="column is-half has-background-white pt-6 px-6">
                     <div className="block">
                         <ArticleContent article={article} />
+                    </div>
+
+                    <div className="block">
+                        <div className="has-text-centered mb-2">
+                            <div className="icon"><i className="fas fa-tag"></i></div>
+                        </div>
+                        <div className="tags is-centered">
+                            {article.tags.map(tag => <Tag tag={tag} isActive={false} handleClick={handleTagClick} />)}
+                        </div>
                     </div>
 
                     <div className="block">
@@ -85,7 +104,7 @@ function Article() {
                                 <i className="fas fa-comments"></i>
                             </span> 全部评论：
                         </h1>
-                        <PageComment commentList={commentList} showArticle={false} currPage={currPage} totalPages={totalPages} handleChangePage={handleChangePage}/>
+                        <PageComment commentList={commentList} showArticle={false} currPage={currPage} totalPages={totalPages} handleChangePage={handleChangePage} />
                     </div>
                 </div>
             </div>
