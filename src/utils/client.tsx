c277880810efdd8966539e2907d3fbf70df0d0e0
wokron <stringcatwok@gmail.com>
2023-10-18 19:37:58 +0800

    feat: jwt support

--- src/utils/client.tsx
+++ src/utils/client.tsx
@@ -1,7 +1,32 @@
 import axios from "axios";
+import Cookies from "js-cookie"
 
 const client = axios.create({
     baseURL: "http://127.0.0.1:8000/"
-})
+});
+
+client.interceptors.request.use(
+    config => {
+        if (Cookies.get("jwt_token")) {
+            config.headers["Authorization"] = "Bearer " + Cookies.get("jwt_token");
+        }
+        return config;
+    },
+    err => {
+        return Promise.reject(err);
+    }
+);
+
+client.interceptors.response.use(
+    response => {
+        return response;
+    }, err => {
+        if (err.response.status == 401) {
+            Cookies.remove("jwt_token")
+        }
+        return err;
+    }
+);
+
 
 export default client;
